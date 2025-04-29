6d24397ae6a6a81398cd8105e2d6d574a5bb98ba
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-04-29 23:39:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // Update CORS configuration for production
 app.use(cors({
-    origin: ['https://soft-entremet-f70f8e.netlify.app/'],
+    origin: ['https://soft-entremet-f70f8e.netlify.app'],
     methods: ['GET', 'POST', 'DELETE'],
     credentials: true
   }));
